081635d17d0fc5eeb30a3d72357b3b6438ddc0df
Andrey <dedok-iu3@yandex.ru>
2022-11-01 22:47:02 +0300

    Проверка уникальности логина

--- server/src/signup.ts
+++ server/src/signup.ts
@@ -36,6 +36,20 @@ export class User extends TypedData {
   }
 }
 
+//Проверка уникальности логина
+async function existsUser(session: Session, login: string): Promise<number> {
+  const query = `
+  ${SYNTAX_V1}
+  DECLARE $login AS Utf8;
+  SELECT * FROM users WHERE login = $login`;
+  const preparedQuery = await session.prepareQuery(query);
+  const { resultSets } = await session.executeQuery(preparedQuery, {
+    $login: TypedValues.utf8(login),
+  });
+  return resultSets[0].rows?.length ?? 0;
+}
+
+//Добавление пользователя в базу данных
 async function upsertUser(session: Session, user: User): Promise<void> {
   const query = `
 ${SYNTAX_V1}
@@ -53,20 +67,34 @@ UPSERT INTO users (id, login, password) VALUES
 }
 
 export const handler: Handler.Http = async (event, context) => {
-  let driver = await initDb();
-  let data: IUser = JSON.parse(event.body);
-  let user = new User(data);
+  const driver = await initDb();
+  const requestData: IUser = JSON.parse(event.body);
+  const user = new User(requestData);
+  let usersCount = 0;
   await driver.tableClient.withSession(async (session) => {
-    await upsertUser(session, user);
+    usersCount = await existsUser(session, requestData.login);
+    if(usersCount == 0)
+        await upsertUser(session, user);
   });
   driver.destroy();
+  if (usersCount > 0) {
+    return {
+      statusCode: 403,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: "Пользователь с таким логином уже зарегистрирован",
+      }),
+    };
+  }
   let secret = process.env.SECRET_KEY as Secret;
-  let token = sign({data: user}, secret, {expiresIn: '2h',});
+  let token = sign({ data: user }, secret, { expiresIn: "2h" });
   return {
     statusCode: 201,
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({token: token}),
+    body: JSON.stringify({ token: token }),
   };
 };
